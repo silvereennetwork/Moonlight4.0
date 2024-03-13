39901f4950b56a19cb737e03da96c81376e96d35
Silvereen <silvereennetwork@gmail.com>
2024-03-13 01:25:22 +0000

    Add ctrl+shift+i for console, hopefully fix icon

--- public/assets/js/render-games.js
+++ public/assets/js/render-games.js
@@ -5,11 +5,6 @@ fetch('assets/js/games.json')
         games.forEach(game => {
             const card = document.createElement('div');
             card.className = `card p`;
-            var gameIcon = getCdnInfo(game.root+"/"+game.img)
-            console.log(gameIcon);
-            card.style = "background-image: url(" + gameIcon + ");";
-            //card.style= "background-image: url(" + gameIcon + "); background-size: cover; background-position: center;";
-            console.log("/assets/imgs/thumbnails/"+game.img);
             card.onclick = function() {
                 setGameInfo(`${game.root}/${game.file}`, game.name);
             };
@@ -23,6 +18,12 @@ fetch('assets/js/games.json')
             const a = document.createElement('a');
             a.href = "iframe.html";
             a.appendChild(card);
+
+            var gameIcon = getCdnInfo(game.root+"/"+game.img)
+            console.log(gameIcon);
+            card.style = "background-image: url(" + gameIcon + ");";
+            //card.style= "background-image: url(" + gameIcon + "); background-size: cover; background-position: center;";
+            console.log("/assets/imgs/thumbnails/"+game.img);
 
             cardsContainer.appendChild(a);
         });
