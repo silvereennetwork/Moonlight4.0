7f813f5330214ca0bcb1d8a4f263f79efb5fa9a3
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-27 12:06:00 -0500

    Update cloak.js

--- public/assets/js/cloak.js
+++ public/assets/js/cloak.js
@@ -45,6 +45,32 @@ function setIcon(icon) {
 
 document.addEventListener("keydown", (e) => {
     if (e.key == localStorage.getItem('moonlight.panicKey')) {
-      window.open(localStorage.getItem('moonlight.panicSite'), '_self')
+        window.open(localStorage.getItem('moonlight.panicSite'), '_self')
     }
 });
+
+let isLeavePopupEnabled = JSON.parse(localStorage.getItem('leavePopupEnabled'));
+
+if (isLeavePopupEnabled === null) {
+    isLeavePopupEnabled = false;
+}
+
+window.addEventListener('beforeunload', function (e) {
+    if (isLeavePopupEnabled) {
+        e.preventDefault();
+        e.returnValue = '';
+    }
+});
+
+document.getElementById('toggleLeavePopup').addEventListener('change', function () {
+    isLeavePopupEnabled = !isLeavePopupEnabled;
+    localStorage.setItem('leavePopupEnabled', JSON.stringify(isLeavePopupEnabled));
+
+    if (isLeavePopupEnabled) {
+        alert('Leave Site Popup Enabled');
+    } else {
+        alert('Leave Site Popup Disabled');
+    }
+});
+
+document.getElementById('toggleLeavePopup').checked = isLeavePopupEnabled;
